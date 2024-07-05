adbd8c69e011a4907c38adca9bc7681a770efcad
suynnn <suy00065@gmail.com>
2024-07-05 22:48:41 +0900

    [feat] 로그인 로직 중 일부를 서비스로 옮김

--- src/main/java/org/mylog/domain/user/controller/LoginController.java
+++ src/main/java/org/mylog/domain/user/controller/LoginController.java
@@ -7,11 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mylog.domain.user.domain.User;
 import org.mylog.domain.user.dto.LoginDto;
-import org.mylog.domain.user.dto.UserLoginResponseDto;
+import org.mylog.domain.user.service.LoginService;
 import org.mylog.domain.user.service.UserService;
-import org.mylog.global.jwt.domain.RefreshToken;
 import org.mylog.global.jwt.service.RefreshTokenService;
-import org.mylog.global.jwt.util.JwtTokenizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -26,9 +24,10 @@ import java.util.List;
 public class LoginController {
 
     private final UserService userService;
+    private final LoginService loginService;
     private final RefreshTokenService refreshTokenService;
+
     private final PasswordEncoder passwordEncoder;
-    private final JwtTokenizer jwtTokenizer;
 
     @GetMapping("/login-form")
     public String loginForm() {
@@ -47,32 +46,9 @@ public class LoginController {
             return "login/login-form";
         }
 
-        List<String> roles = user.getUserRoles()
-                .stream().map(role -> role.getRole().getRoleEnum().name()).toList();
-
-        String accessToken = jwtTokenizer.createAccessToken(
-                user.getUserId(),
-                user.getEmail(),
-                user.getName(),
-                user.getId(),
-                roles
-        );
-
-        String refreshToken = jwtTokenizer.createRefreshToken(user.getUserId(), user.getEmail());
-
-        // 리프레시 토큰을 디비에 저장
-        RefreshToken refreshTokenEntity = new RefreshToken();
-        refreshTokenEntity.setValue(refreshToken);
-        refreshTokenEntity.setUserId(user.getUserId());
-
-        refreshTokenService.addRefreshToken(refreshTokenEntity);
-
-        UserLoginResponseDto loginResponseDto = UserLoginResponseDto.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .userId(user.getUserId())
-                .name(user.getName())
-                .build();
+        List<String> tokens = loginService.login(user);
+        String accessToken = tokens.get(0);
+        String refreshToken = tokens.get(1);
 
         Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
         accessTokenCookie.setHttpOnly(true); // 보안 (쿠키값을 자바스크립트 같은 곳에서는 접근 불가)
@@ -93,6 +69,7 @@ public class LoginController {
                          HttpServletResponse response) {
 
         Cookie[] cookies = request.getCookies();
+
         for (Cookie cookie : cookies) {
             if ("refreshToken".equals(cookie.getName())) {
                 if (refreshTokenService.findRefreshToken(cookie.getValue()).isPresent()) {
