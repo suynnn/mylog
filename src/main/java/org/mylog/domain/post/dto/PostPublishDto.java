54e9000bd30ff002ccc8f19c68c32a7607b21939
suynnn <suy00065@gmail.com>
2024-07-05 22:31:21 +0900

    [feat] Post 등록 dto 작성

---
+++ src/main/java/org/mylog/domain/post/dto/PostPublishDto.java
@@ -0,0 +1,23 @@
+package org.mylog.domain.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class PostPublishDto {
+    private String title;
+    private String content;
+    private Boolean isTemp;
+    private Boolean isPrivate;
+    private String thumbnailUrl;
+    private String username;
+    private List<String> series;
+    private List<MultipartFile> images;
+    private List<String> tags;
+}
