7e0a4ad92fdb691d6388707f7fd0050526d8e6c8
suynnn <suy00065@gmail.com>
2024-07-08 10:15:19 +0900

    [feat] api로 태그 등록하는 코드 작성

--- src/main/java/org/mylog/domain/tag/repository/TagRepository.java
+++ src/main/java/org/mylog/domain/tag/repository/TagRepository.java
@@ -4,6 +4,10 @@ import org.mylog.domain.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByName(String name);
 }
--- src/main/java/org/mylog/domain/tag/service/impl/TagServiceImpl.java
+++ src/main/java/org/mylog/domain/tag/service/impl/TagServiceImpl.java
@@ -1,13 +1,31 @@
 package org.mylog.domain.tag.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.mylog.domain.tag.domain.Tag;
 import org.mylog.domain.tag.repository.TagRepository;
 import org.mylog.domain.tag.service.TagService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TagServiceImpl implements TagService {
 
     private final TagRepository tagRepository;
+
+    @Override
+    public Tag saveTag(String name) {
+        Optional<Tag> tagFindWithName = tagRepository.findByName(name);
+
+        if (tagFindWithName.isPresent()) {
+            return tagFindWithName.get();
+        }
+
+        Tag tag = Tag.builder()
+                .name(name)
+                .build();
+
+        return tagRepository.save(tag);
+    }
 }
