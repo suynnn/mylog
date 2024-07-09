abe696c5935de143745d46967292a8bab036aaa9
suynnn <suy00065@gmail.com>
2024-07-09 11:26:56 +0900

    [feat] 모달창에서 썸네일 업로드 구현

--- src/main/java/org/mylog/domain/post/dto/PostPublishDto.java
+++ src/main/java/org/mylog/domain/post/dto/PostPublishDto.java
@@ -18,7 +18,7 @@ public class PostPublishDto {
     private MultipartFile thumbnail;
     private Long userId;
     private Long blogId;
-    private List<String> series;
+    private String series;
     private List<MultipartFile> images;
     private List<String> tags;
 }
